6004a36c94796a7b5759a45d9371173311478f04
Kashyap Sukshavasi <101945096+kashsuks@users.noreply.github.com>
2024-03-05 20:06:40 -0500

    Delete script.js
    deleted script.js and implemented js code into html

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const juniorLink = document.getElementById('junior-link');
-    const juniorSection = document.getElementById('junior');
-
-    juniorLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        juniorSection.classList.toggle('hidden');
-    });
-});
